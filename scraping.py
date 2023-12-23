db089904886005a5d2e6ba48f0d3f73bb2f8ba14
Rafael pereira de oliveira <raffasadol@live.com>
2023-12-23 11:42:23 -0300

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -9,32 +9,11 @@ hora_extracao = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 bsp_texto = BeautifulSoup(texto_string, 'html.parser')
 lista_noticias = bsp_texto.find_all('div', attrs={'class':'feed-post-body'})
 print("Quantidade de manchetes = ", len(lista_noticias))
-lista_noticias[0].contents
-
-lista_noticias[0].contents[1].text.replace('"',"")
-lista_noticias[0].find('a').get('href')
-
-descricao = lista_noticias[0].contents[2].text
-if not descricao:
-    descricao = lista_noticias[0].find('div', attrs={'class': 'bstn-related'})
-    descricao = descricao.text if descricao else None # Somente acessará a propriedade text caso tenha encontrado ("find")
-descricao
-
-metadados = lista_noticias[0].find('div', attrs={'class':'feed-post-metadata'})
-
-time_delta = metadados.find('span', attrs={'class': 'feed-post-datetime'})
-secao = metadados.find('span', attrs={'class': 'feed-post-metadata-section'})
-
-time_delta = time_delta.text if time_delta else None
-secao = secao.text if secao else None
-    
-print('time_delta = ', time_delta)
-print('seção = ', secao)
 
 dados = []
 
-for noticia in lista_noticias:
-    manchete = noticia.contents[1].text.replace('"',"")
+for i, noticia in enumerate(lista_noticias, start=1):
+    manchete = noticia.contents[1].text.replace('"', "")
     link = noticia.find('a').get('href')
 
     descricao = noticia.contents[2].text
@@ -50,6 +29,13 @@ for noticia in lista_noticias:
     secao = secao.text if secao else None
 
     dados.append((manchete, descricao, link, secao, hora_extracao, time_delta))
+
+    print(f"\n### Manchete {i} ###")
+    print(f"Manchete: {manchete}")
+    print(f"Link: {link}")
+    print(f"Descrição: {descricao}")
+    print(f"Seção: {secao}")
+    print(f"Tempo da postagem: {time_delta}")
 
 df = pd.DataFrame(dados, columns=['manchete', 'descrição', 'link', 'seção', 'hora_extração', 'time_delta'])
 df.head()
